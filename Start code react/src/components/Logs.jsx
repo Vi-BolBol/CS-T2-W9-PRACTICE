384ae4ab851f37ab604e9856bb8cf3250f875133
BolBol <chhonchhaychanvibol@gmail.com>
2026-03-19 15:02:03 +0700

    Organize  Logs.jsx

--- Start code react/src/components/Logs.jsx
+++ Start code react/src/components/Logs.jsx
@@ -1,22 +1,19 @@
-export default function Logs({logs}){
-    return (
-        <section className="container" id="log">
-            <h2>Battle Log</h2>
-            <ul>
-                {logs.map((log, index) => (
-                    <li key={index}>
-                        <span className={log.isPlayer ? "log--player" : "log--monster"}>
-                            {log.isPlayer ? "Player" : "Monster"}
-                        </span>
-                        <span>
-                            {log.text}
-                            <span className={log.isDamage ? "log--damage" : "log--heal"}>
-                            {log.value}
-                            </span>
-                        </span>
-                    </li>
-                ))};
-            </ul>
-        </section>
-    );
+export default function Logs({ logs }) {
+  return (
+    <section id="log" className="container">
+      <h2>Battle Log</h2>
+      <ul>
+        {logs.map((log, index) => (
+          <li key={index}>
+            <span className={log.isPlayer ? "log--player" : "log--monster"}>
+              {log.isPlayer ? "Player" : "Monster"}
+            </span>
+            <span className={log.isDamage ? "log--damage" : "log--heal"}>
+              {" "}{log.text}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
