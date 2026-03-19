798f5d1677ec80fcf11b6ccb3acb619b3b9d4c14
BolBol <chhonchhaychanvibol@gmail.com>
2026-03-19 14:47:05 +0700

    Add GameOver.jsx

---
+++ Start code react/src/components/GameOver.jsx
@@ -0,0 +1,9 @@
+export default function GameOver({title, restartGame}){
+    return(
+        <section className="container">
+            <h2>Game Over!</h2>
+            <h3>{title}</h3>
+            <button onClick={restartGame}>Start New Game</button>
+        </section>
+    );
+}
